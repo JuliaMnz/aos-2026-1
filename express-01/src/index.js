48c59278ce435811a452592ab27b10802eba74be
Júlia Muniz <julia.mco0512@gmail.com>
2026-03-02 22:10:06 +0000

    preparando para deploy

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello Project.");
+import "dotenv/config";
+import express from "express";
+npm
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor rodando!\n" + process.env.MESSAGE);
+});
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () =>
+  console.log(
+    "Example app listening on port " + port + "!\n" + process.env.MESSAGE
+  )
+);
