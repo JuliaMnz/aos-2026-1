8971b2c84779a625cdd86a300f4d81506aefecfb
Júlia Muniz <julia.mco0512@gmail.com>
2026-03-23 23:16:38 +0000

    continuando atividade

---
+++ express-01/api/routes/message.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const router = Router();
+
+//GET /messages - Listar todas as mensagens
+router.get("/", async (req, res) => {
+  const messages = await req.context.models.Message.findAll();
+  return res.send(messages);
+});
+
+//GET
+router.get("/:messageId", async (req, res) => {
+  const message = await req.context.models.Message.findByPk(
+    req.params.messageId,
+  );
+  return res.send(message);
+});
+
+//POST
+router.post("/", async (req, res) => {
+  const message = await req.context.models.Message.create({
+    text: req.body.text,
+    userId: req.context.me.id,
+  });
+
+  return res.send(message);
+});
+
+//POST /messages
+
+router.delete("/:messageId", async (req, res) => {
+  const result = await req.context.models.Message.destroy({
+    where: { id: req.params.messageId },
+  });
+
+  return res.send(true);
+});
+
+export default router;
