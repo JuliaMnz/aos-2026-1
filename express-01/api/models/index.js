276528f3649194da564046ed465d40db94d483f3
Júlia Muniz <julia.mco0512@gmail.com>
2026-03-24 18:56:12 +0000

    Instalando dependências de banco e cors

--- express-01/api/models/index.js
+++ express-01/api/models/index.js
@@ -1,10 +1,11 @@
 import Sequelize from "sequelize";
 import getUserModel from "./user";
-import getMessageModel from './message';
+import getMessageModel from "./message";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
