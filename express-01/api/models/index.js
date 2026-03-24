c5b97e4b2973a4de104956d382a008982a69a589
Júlia Muniz <julia.mco0512@gmail.com>
2026-03-24 19:01:03 +0000

    Dependências instaladas e configuradas

--- express-01/api/models/index.js
+++ express-01/api/models/index.js
@@ -1,6 +1,7 @@
 import Sequelize from "sequelize";
 import getUserModel from "./user";
 import getMessageModel from "./message";
+import pg from 'pg';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
