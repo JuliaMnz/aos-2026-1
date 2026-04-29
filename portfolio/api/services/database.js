cf79e453c0d944accc6c5dd2a8a9c285bbbb8562
Júlia Muniz <julia.mco0512@gmail.com>
2026-04-29 19:49:11 +0000

    .

--- portfolio/api/services/database.js
+++ portfolio/api/services/database.js
@@ -4,9 +4,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: { require: true, rejectUnauthorized: false }
-  },
-  logging: false
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  }
 });
 
 module.exports = sequelize;
