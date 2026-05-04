6a3cfaef0fb7f5eaca00e4af53b02526fe843e7f
Júlia Muniz <julia.mco0512@gmail.com>
2026-05-04 22:02:30 +0000

    fix: corrigindo o modelo pessoa que estava duplicado

--- portfolio/api/models/pessoa.js
+++ portfolio/api/models/pessoa.js
@@ -1,11 +1,18 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../services/database'); 
+const sequelize = require('../services/database');
 
 const Pessoa = sequelize.define('Pessoa', {
-  nome: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  bio: { type: DataTypes.TEXT },
-  formacao: { type: DataTypes.STRING }
+  nome: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  },
+  cargo: { 
+    type: DataTypes.STRING 
+  },
+  biografia: { 
+    type: DataTypes.TEXT 
+  }
+  // Se você tiver outros campos (como idade, email), pode adicionar aqui!
 });
 
 module.exports = Pessoa;
