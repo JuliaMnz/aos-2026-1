2b8bd7aab061345fbf9d63f6714a9a97a427115a
Júlia Muniz <julia.mco0512@gmail.com>
2026-05-05 02:21:29 +0000

    fix: adicionando dialectModule para resolver erro de driver na vercel

--- portfolio/api/services/database.js
+++ portfolio/api/services/database.js
@@ -1,19 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-// Só carrega o dotenv se não estiver na Vercel (onde as variáveis já existem)
+// Só carrega o dotenv se não estiver na Vercel
 if (!process.env.VERCEL) {
     require('dotenv').config();
 }
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  logging: false
+    dialect: 'postgres',
+    // ESTA LINHA É A CHAVE: Força a Vercel a usar o pacote 'pg' instalado
+    dialectModule: require('pg'), 
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
 });
 
 module.exports = sequelize;
